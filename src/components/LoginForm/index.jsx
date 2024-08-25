13e18b96bceb59440fe9f5bdf33a816b8eddfd7e
balhan84 <balamutenkoanna@gmail.com>
2024-08-25 23:44:02 +0300

    Add validation input

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,6 +1,8 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import { LOGIN_FORM_SCHEMA } from "../../utils/validationSchemas";
+import classNames from "classnames";
 import styles from "./LoginForm.module.sass";
+
 function LoginForm() {
   const initialValues = { login: "", password: "" };
 
@@ -15,45 +17,59 @@ function LoginForm() {
       onSubmit={handleSubmit}
       validationSchema={LOGIN_FORM_SCHEMA}
     >
-      {(formikProps) => (
-        <Form className={styles.loginForm}>
-          <h2 className={styles.loginTitle}>LOGIN TO YOUR ACCOUNT</h2>
-          <div>
-            <label className={styles.loginLabel}>
-              <Field
-                className={styles.loginInput}
-                type="text"
+      {(formikProps) => {
+        const loginNameClass = classNames(styles.loginInput, {
+          [styles.valid]:
+            !formikProps.errors.login && formikProps.touched.login,
+          [styles.invalid]:
+            formikProps.errors.login && formikProps.touched.login,
+        });
+        const passwordNameClass = classNames(styles.loginInput, {
+          [styles.valid]:
+            !formikProps.errors.password && formikProps.touched.password,
+          [styles.invalid]:
+            formikProps.errors.password && formikProps.touched.password,
+        });
+        return (
+          <Form className={styles.loginForm}>
+            <h2 className={styles.loginTitle}>LOGIN TO YOUR ACCOUNT</h2>
+            <div>
+              <label className={styles.loginLabel}>
+                <Field
+                  className={loginNameClass}
+                  type="text"
+                  name="login"
+                  placeholder="Your Login"
+                  autoFocus
+                />
+              </label>
+              <ErrorMessage
                 name="login"
-                placeholder="Your Login"
-                autoFocus
+                component="span"
+                className={styles.loginError}
               />
-            </label>
-            <ErrorMessage
-              name="login"
-              component="span"
-              className={styles.loginError}
-            />
-          </div>
-          <div>
-            <label className={styles.loginLabel}>
-              <Field
-                className={styles.loginInput}
-                type="text"
+            </div>
+            <div>
+              <label className={styles.loginLabel}>
+                <Field
+                  className={passwordNameClass}
+                  type="text"
+                  name="password"
+                  placeholder="Your Password"
+                />
+              </label>
+              <ErrorMessage
                 name="password"
-                placeholder="Your Password"
+                component="span"
+                className={styles.loginError}
               />
-            </label>
-            <ErrorMessage
-              name="password"
-              component="span"
-              className={styles.loginError}
-            />
-          </div>
-          <button type="submit" className={styles.loginBtn}>
-            LOGIN
-          </button>
-        </Form>
-      )}
+            </div>
+            <button type="submit" className={styles.loginBtn}>
+              LOGIN
+            </button>
+          </Form>
+        );
+      }}
     </Formik>
   );
 }
